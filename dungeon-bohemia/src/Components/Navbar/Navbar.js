42e43c010c77a8d282b857b612459359ba339ff5
SpencerFaust <faus0059@umn.edu>
2019-05-13 10:36:19 -0500

    Playing with nav drop downs using CSS.

--- dungeon-bohemia/src/Components/Navbar/Navbar.js
+++ dungeon-bohemia/src/Components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import icon from '../../Images/Menu.png';
 
 class Navbar extends Component {
     render() {
 
     return (
-        <div className="navbar"> 
-                <Link className="links" to="../Projects">Projects</Link>
-                <Link className="links" to="../About">About</Link>
-                <Link className="links" to="../Home">Home</Link>
+
+        
+        <div class="navbar">
+            <div class="dropdown">
+                <button class="dropbtn">
+                    <img alt="menu button" src={icon} id="menu-button" />   
+                </button>
+                <div class="dropdown-content">
+                    <Link className="links" to="../Projects">Projects</Link>
+                    <Link className="links" to="../About">About</Link>
+                    <Link className="links" to="../Home">Home</Link> 
+                </div>
+            </div> 
         </div>
     );
     };
